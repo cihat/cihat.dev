790722ab72742c0a0fe82b2045c45696a0879e1a
Cihat Salik <cihatsalik1@gmail.com>
2023-06-10 04:00:08 +0300

    Add some helper function

---
+++ lib/meta.ts
@@ -0,0 +1,14 @@
+export const META_DATA = {
+  title: "Cihat Salik",
+  description: "Art lover, developer, creator and photographer",
+  url: "https://cihat.dev",
+  email: "cihatsalik1@gmail.com",
+  name: "Cihat Salik",
+  social: {
+    vsco: "https://vsco.co/cihqt/gallery",
+    instagram: "https://instagram.com/cihat.png",
+    twitter: "https://twitter.com/chtslk",
+    github: "https://github.com/cihat",
+  },
+  locale: "tr-TR",
+};
