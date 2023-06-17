335d1aff51928935ff8e456e07f574c4a598911c
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-06-17 21:45:22 +0300

    Update next config for opengraph images on `/projects` page

--- next.config.js
+++ next.config.js
@@ -11,7 +11,12 @@ const nextConfig = {
     ]
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'opengraph.githubassets.com',
+      },
+    ],
   },
 }
 
