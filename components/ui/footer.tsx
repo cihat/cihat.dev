24dad1ca19206993759c4ce31e18ab73321f3d39
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-06-17 17:42:05 +0300

    Update footer and add custom `a` element

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,9 +1,20 @@
-import Container from "@/components/ui/container";
+import A from "@/components/ui/a"
 
 export default function Footer() {
   return (
-    <Container className="flex justify-center select-none">
-      <h1>Cihat Salik © 2023</h1>
-    </Container>
+    <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500">
+      <div className="grow text-left">
+        Cihat Salik (
+        <A target="_blank" href="https://twitter.com/chtslk">
+          @chtslk
+        </A>
+        )
+      </div>
+      <div>
+        <A target="_blank" href="https://github.com/cihat/cihat.dev">
+          Source
+        </A>
+      </div>
+    </footer>
   )
 }
