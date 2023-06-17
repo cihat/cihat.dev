10072c156f98d13edcaf52690a55993d748bf049
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-06-17 22:34:52 +0300

    Add animation to projects component

--- app/projects/projectCards.tsx
+++ app/projects/projectCards.tsx
@@ -13,7 +13,7 @@ function Card(props: CardType) {
   const { link, description, forks, image, language, languageColor, owner, repo, stars, website } = props.project
 
   return (
-    <div className="flex flex-col p-4 border radious rounded-md h-max" key={props.key}>
+    <div className="project-card flex flex-col p-4 border radious rounded-md h-max" key={props.key}>
       <div className="flex">
         <A href={link} target="_blank" className="font-bold hover:text-[#2f81f7] dark:hover:text-[#2f81f7]">
           {repo}
@@ -22,21 +22,25 @@ function Card(props: CardType) {
       <div className="mt-2">
         <p>{description}</p>
       </div>
-      <div className="flex items-center mt-2">
+      <div className="flex items-center my-2">
         <span className="flex justify-start items-center mr-2">
           <span className="w-3 h-3 rounded-full mr-1"
             style={{ backgroundColor: !languageColor ? "#f1e05a" : `${languageColor}` }}
           />
           <p>{language}</p>
         </span>
-        <A href={`${link}/stargazers`} target="_blank" className="flex items-center mr-2  border-none hover:text-[#2f81f7] dark:hover:text-[#2f81f7]">
-          <AiOutlineStar className="mr-0.5" />
-          {stars}
-        </A>
-        <A href={`${link}/forks`} target="_blank" className="flex items-center border-none hover:text-[#2f81f7] dark:hover:text-[#2f81f7]">
-          <AiOutlineBranches className="mr-0.5" />
-          {forks}
-        </A>
+        {
+          stars > 0 ? <A href={`${link}/stargazers`} target="_blank" className="flex items-center mr-2  border-none hover:text-[#2f81f7] dark:hover:text-[#2f81f7]">
+            <AiOutlineStar className="mr-0.5" />
+            {stars}
+          </A> : null
+        }
+        {
+          forks > 0 ? <A href={`${link}/forks`} target="_blank" className="flex items-center border-none hover:text-[#2f81f7] dark:hover:text-[#2f81f7]">
+            <AiOutlineBranches className="mr-0.5" />
+            {forks}
+          </A> : null
+        }
       </div>
       <Image src={image} width={500} height={250} alt={repo} loading="lazy" />
     </div>
@@ -51,7 +55,7 @@ export default function ProjectCards(props: ProjectCards) {
   const { pinnedProjects } = props
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 top-animation">
       {
         pinnedProjects.map((project: any) =>
           <Card project={project} key={project.repo} />)
