717d320fa3bf22fb64b12e3543f3651e75ab3d98
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-06-20 20:15:31 +0300

    Fix type error from TS on upstash redis's config

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,10 @@
 import { Redis } from "@upstash/redis"
 
-const redis = new Redis({
+const config = {
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN
-})
+} as any
+
+const redis = new Redis(config)
 
 export default redis
