1c48e1dbf3be8b33a264c96e5978be1eeeb49a1f
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-06-24 23:40:42 +0300

    Add `quote` component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Container from "@/components/ui/container";
 import { Posts } from "./posts";
 import { getPosts } from "@/lib/get-posts";
+import { RandomQuote } from '@/components/ui/quote'
 
 export default async function Home() {
   const posts = await getPosts();
 
-  return <Posts posts={posts} />
+  return (
+    <>
+      <Posts posts={posts} />
+      <RandomQuote />
+    </>
+  )
 }
