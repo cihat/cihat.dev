1f4b39b9fa256e931742551b386310c4c8a9e6cc
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-06-27 02:17:34 +0300

    Add my first blog post and other two posts but  just title

--- lib/fetch-stoic-quote.ts
+++ lib/fetch-stoic-quote.ts
@@ -3,7 +3,7 @@ import { initalQuote } from "@/lib/meta"
 export default async function getQuote() {
   const stoicApiPath = process.env.STOIC_API_PATH as string
 
-  if (process.env.NODE_ENV === "development") return initalQuote
+  // if (process.env.NODE_ENV === "development") return initalQuote
 
   return await fetch("https://stoic-quotes.com/api/quote", { method: "GET" })
     .then(res => res.json())
