786bed4a651d0ea17161c4052c182064eaea9dae
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-07-16 15:14:00 +0300

    Add reading and learning empty files and dummy blog

---
+++ app/learning/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/ui/container";
+
+export default function Learning() {
+  return (
+    <Container className="px-0" >
+      <h1>Coming Soon</h1>
+    </Container>
+  )
+}
---
+++ app/reading/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/ui/container";
+
+export default function Home() {
+  return (
+    <Container className="px-0" >
+      <h1>Coming Soon</h1>
+    </Container>
+  )
+}
