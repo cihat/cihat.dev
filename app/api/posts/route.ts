0f4871d04dd2682a3b99080b22a69769ff8b3f1b
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-07-23 18:31:42 +0300

    Update dynamic type to `force-dynamic`

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getPostsWithViewData } from "@/lib/get-posts";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   return NextResponse.json(await getPostsWithViewData());
