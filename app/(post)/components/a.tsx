feb5deb5e079434644173099c2995fe5b11669e2
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-02-05 14:27:23 +0300

    Fix link target option

--- app/(post)/components/a.tsx
+++ app/(post)/components/a.tsx
@@ -14,6 +14,7 @@ export function A({ children, className = "", href, ...props }) {
   } else {
     return (
       <Link
+        target="_blank"
         href={href}
         className={`border-b text-gray-600 border-gray-300 transition-[border-color] hover:border-gray-600 dark:text-white dark:border-gray-500 dark:hover:border-white ${className}`}
         {...props}
