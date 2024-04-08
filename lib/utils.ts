572b7f85d509828b2408a5eba2580896a68ae023
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-08 23:31:01 +0300

    Separate getYear function to lib

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
+export function getYear(date: string) {
+  return new Date(date).getFullYear();
+}
