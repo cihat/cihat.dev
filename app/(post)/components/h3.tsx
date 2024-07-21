3d5270772a10aaae658ecf24d37f5a821c98059b
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-07-21 23:10:51 +0300

    Update spacing in h3 elements

--- app/(post)/components/h3.tsx
+++ app/(post)/components/h3.tsx
@@ -2,7 +2,7 @@ import { withHeadingId } from "./utils";
 
 export function H3({ children }) {
   return (
-    <h3 className="group font-bold text-lg my-4 relative">
+    <h3 className="group font-bold text-lg mb-2 mt-4 relative">
       {withHeadingId(children)}
     </h3>
   );
