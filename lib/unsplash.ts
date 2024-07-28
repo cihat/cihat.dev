90287a8583e3e9e4b2541f341ef627f4859f8e56
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-07-28 18:21:15 +0300

    Refactor unsplash class

--- lib/unsplash.ts
+++ lib/unsplash.ts
@@ -4,6 +4,8 @@ import { dummyPhoto } from "./meta";
 class Unsplash {
   private base_url: string = "https://api.unsplash.com";
   private client_id: string = `?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+  private uiUxKeywords: string = "natural,landscape,designer,design,art,artist,illustration,illustrator,graphic,graphic design,graphic designer,web,web design,web designer,ui,ux,ui/ux,ui design,ux design,ui/ux design,ui designer";
+
   public photo: IRandomPhoto = dummyPhoto
 
   async getData(url): Promise<IRandomPhoto> {
@@ -15,8 +17,8 @@ class Unsplash {
   }
 
   async getRandomPhoto(): Promise<IRandomPhoto> {
-    const url = `${this.base_url}/photos/random/${this.client_id}&query=natural,landscape,designer,design,art,artist,illustration,illustrator,graphic,graphic design,graphic designer,web,web design,web designer,ui,ux,ui/ux,ui design,ux design,ui/ux design,ui designer`;
-    // if (!this.photo?.urls || process.env.NODE_ENV !== "development") return await this.getData(url)
+    const url = `${this.base_url}/photos/random/${this.client_id}&query=${this.uiUxKeywords}`;
+
     if (process.env.NODE_ENV !== "development") return await this.getData(url)
     return dummyPhoto
   }
