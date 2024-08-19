8b6ea3a6db0c05dbbf28840bca567001344daed1
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-08-20 00:23:29 +0300

    Update comment component to use `next/navigation` for pathname

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,23 +1,24 @@
 // components/CommentSection.js
 "use client";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import postsData from "@/lib/posts.json";
 
 const CommentSection = () => {
-  //TODO: issueNumber as dynamic value
-  // const issueNumber = "2";
+  const pathname = usePathname();
+
+  const postPath = pathname.split("/").pop();
+  const issueNumber = postsData.posts?.find((post) => post.path === postPath)?.issueNumber;
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://utteranc.es/client.js";
     script.setAttribute("repo", "cihat/cihat.dev"); // GitHub repo URL'si
     script.setAttribute("issue-term", "pathname"); // Yorumları URL'ye göre gruplamak için
-    script.setAttribute("theme", "github-light"); // Tema seçimi
+    script.setAttribute("theme", "preferred-color-scheme"); // Tema seçimi
     script.setAttribute("crossOrigin", "anonymous");
     script.setAttribute("async", "true");
-    script.setAttribute("issue-number", "2");
-
-    // Eğer issueNumber mevcutsa, bunu kullan
-    // if (issueNumber) {
-    // }
+    script.setAttribute("issue-number", `${issueNumber}`);
 
     // @ts-ignore
     document.getElementById("utterances").appendChild(script);
