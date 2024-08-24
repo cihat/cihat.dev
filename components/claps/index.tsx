bbb8528575f845b2b9de856ea96afa527d2bdc8c
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-08-24 20:17:52 +0300

    Update ClapsButton component to use useIsMobile hook
    This commit updates the ClapsButton component by adding the useIsMobile hook to determine the position of the Claps component. The fixed prop is now set to "center" when the device is mobile, and "right" otherwise. This improves the responsiveness of the component and ensures it is displayed correctly on different devices.

--- components/claps/index.tsx
+++ components/claps/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import useIsMobile from "@/hooks/useIsMobile";
 import Claps from "./claps";
 
 export default function ClapsButton() {
-  return <Claps fixed="center" />;
+  const isMobile = useIsMobile();
+  return <Claps fixed={isMobile ? "center" : "right"} />;
 }
