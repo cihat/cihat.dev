fa62839c3c51738f8a8b9f0b428e02cd24e6a5ec
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-08-24 19:46:21 +0300

    Add `useIsMobile` hook for detecting mobile devices

---
+++ hooks/useIsMobile.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
