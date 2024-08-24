5d7f0f3feeeb510e27283ca269b0229f61f0f842
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-08-24 19:54:12 +0300

    Refactor footer component to update link URL

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="py-4 flex text-xs text-center dark:text-gray-400 text-gray-500">
       <div className="grow text-center">
-        <A target="_blank" href="https://twitter.com/chtslk">
+        <A target="_blank" href="https://github.com/cihat">
           Cihat Salik
         </A>
         &nbsp;© 2023
