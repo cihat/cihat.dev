c4910df71e8fa6b79746d532fdf5de78b73d7c68
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-08-24 20:16:52 +0300

    Simplify Logo component and improve code readability

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,9 @@
-"use client";
 import Link from "next/link";
-import { Button } from "./ui/button";
 
-export function Logo() {
-  return (
-    <Link href="/">
-      <Button className="text-md md:text-lg whitespace-nowrap font-bold" variant={"ghost"}>
-        Cihat Salik
-      </Button>
-    </Link>
-  );
-}
+const Logo = () => (
+  <Link href="/" className="text-md text-lg whitespace-nowrap font-bold">
+    Cihat Salik
+  </Link>
+);
+
+export default Logo;
