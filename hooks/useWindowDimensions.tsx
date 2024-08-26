ef1bf2036481233d1b49103006b528ee8d987c4c
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-08-26 21:10:10 +0300

    Refactor useWindowDimensions hook to use useLayoutEffect instead of useEffect

--- hooks/useWindowDimensions.tsx
+++ hooks/useWindowDimensions.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useLayoutEffect, useState } from "react"
 
 function getWindowDimensions() {
   if (typeof window !== "undefined") {
@@ -11,7 +11,7 @@ function getWindowDimensions() {
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions())
     }
