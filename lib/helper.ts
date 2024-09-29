c86d0eb419033137653a7d29c7fd2e1ebe6a0a92
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-29 12:18:22 +0300

    Add new groupBy function

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,8 +1,16 @@
 import { format, parseISO } from "date-fns";
 import { ILink } from "@/types";
 
+//https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
+const groupBy = function (xs, key) {
+  return xs.reduce(function (rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+
 const bookmarkGroupByWeekNumber = (data) => {
-  return Object.groupBy(data, (bookmark: ILink) =>
+  return groupBy(data, (bookmark: ILink) =>
     format(parseISO(bookmark.created), "d MMM yyyy")
   );
 };
