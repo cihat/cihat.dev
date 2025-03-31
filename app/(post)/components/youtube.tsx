4bc22211301050e46f85d9c6ddc5ca25a18b1309
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2025-04-01 00:15:34 +0300

    Enhance YouTube component to allow custom options for video rendering

--- app/(post)/components/youtube.tsx
+++ app/(post)/components/youtube.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import YT from "react-youtube";
 
 export function YouTube(props: any) {
   return (
     <div className="block my-5 overflow-scroll">
-      <YT {...props} opts={{ width: "100%" }} />
+      <YT {...props} opts={{ width: "100%", ...props.opts }} />
     </div>
   );
 }
