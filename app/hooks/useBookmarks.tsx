545cea84251372f5a1b93140551b4d74c610d61d
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2025-10-04 01:08:41 +0300

    Fix date range calculation in useBookmarks hook by adjusting the 'from' date to ensure accurate time range selection. This change addresses a bug with the previous implementation.

--- app/hooks/useBookmarks.tsx
+++ app/hooks/useBookmarks.tsx
@@ -54,7 +54,7 @@ export function useBookmarks(initialTab, initialTimeRange) {
   const [timeRange, setTimeRange] = useState(initialTimeRange);
   const [searchInput, setSearchInput] = useState("");
   const [dateRange, setDateRange] = useState({
-    from: subWeeks(new Date(), 1),
+    from: addDays(subWeeks(new Date(), 1), -1),
     //! Fix better this bug, i add one day for hot fix but it's not the best solution
     to: addDays(new Date(), 1),
   });
